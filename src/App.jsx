2bcbd3c4b336efea542310497afb5693ead741f1
Diana Maria <diana.maria.cruz.c@gmail.com>
2024-12-12 02:40:17 -0600

    NavBar dentro de App
    Se cambia la manera de invocar el NavBar.jsx, queda como hijo de App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/iconMagicNatural.svg'
 import './App.css'
-import Button from './componentes/Button'
+import NavBar from './componentes/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <section className='text-center'>
-        <h1 className='d-none'>Magic Natural</h1>
-        <h2 className='d-none'>Organic skincare</h2>
-        <img src="./public/banner_01.jpg" alt="Bienvenida a Magic Natural" className='banner-item' />
-      </section>
+      <header>
+        <NavBar></NavBar>
+      </header>
+      <main>
+        <section className='text-center'>
+          <h1 className='d-none'>Magic Natural</h1>
+          <h2 className='d-none'>Organic skincare</h2>
+          <img src="./public/banner_01.jpg" alt="Bienvenida a Magic Natural" className='banner-item' />
+        </section>
+      </main>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,11 @@ import './index.css'
 import App from './App.jsx'
 import NavBar from './componentes/NavBar'
 
-createRoot(document.getElementById('header')).render(
+/* createRoot(document.getElementById('header')).render(
   <StrictMode>
     <NavBar />
   </StrictMode>,
-)
+) */
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
