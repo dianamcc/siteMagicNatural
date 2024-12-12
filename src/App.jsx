a6e8a439af5a6431212102e77b11de618241af3b
Diana Maria <diana.maria.cruz.c@gmail.com>
2024-12-12 02:48:35 -0600

    SliderHome como componente
    Se crea el componente SliderHome.jsx y se agrega como hijo de App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/iconMagicNatural.svg'
+
 import './App.css'
+
 import NavBar from './componentes/NavBar'
+import SliderHome from './SliderHome'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,11 +17,7 @@ function App() {
         <NavBar></NavBar>
       </header>
       <main>
-        <section className='text-center'>
-          <h1 className='d-none'>Magic Natural</h1>
-          <h2 className='d-none'>Organic skincare</h2>
-          <img src="./public/banner_01.jpg" alt="Bienvenida a Magic Natural" className='banner-item' />
-        </section>
+        <SliderHome></SliderHome>
       </main>
     </>
   )
---
+++ src/SliderHome.jsx
@@ -0,0 +1,10 @@
+//Componentes de clases
+
+// * PROPS
+export default function SliderHome(props){
+    return <section className='text-center'>
+    <h1 className='d-none'>Magic Natural</h1>
+    <h2 className='d-none'>Organic skincare</h2>
+    <img src="./public/banner_01.jpg" alt="Bienvenida a Magic Natural" className='banner-item' />
+  </section>;
+}
